17fe53b448460448047830e6425656c0c8d30db2
Becca <rln0005@auburn.edu>
2021-04-11 20:09:03 -0700

    adding multiple map/consensus packages to compare

--- minION_align.sh
+++ minION_align.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-####ONLY NEEDED IF USING ASC
-#source /opt/asn/etc/asn-bash-profiles-special/modules.sh
-#module load fastqc/0.10.1
-#module load gnu_parallel/201612222
-#module load gnu_parallel
+################################# minION_align.sh created by Rebecca Nance ########################################
+
 
 #move into data directory
 cd ../minION_align/data
@@ -22,31 +19,51 @@ done<list
 mv *_all.fastq ../
 cd ../
 
-#use NanoFilt to filter reads (based on avg length=500, quality=10, remove first & last 50 bp reads)
-NanoFilt -l 500 -q 12 --headcrop 100 --tailcrop 100 *_all.fastq > trim.fastq
+#use NanoFilt to filter reads (based on avg length=500, quality=12, remove first 50 bp reads)
+NanoFilt -l 500 -q 12 --headcrop 50 < *_all.fastq > trim.fastq
+#### fastqc analysis shows these reads have fairly lousy quality scores (this is as much trimming as can be done)
 
-#use minimap2 to map to reference genome
-minimap2 -ax map-ont --MD *_ref.fasta trim.fastq > mapped.sam
 
-#use samtools to convert sam to bam
-samtools view -bS mapped.sam > mapped.bam
-#use samtools to sort the bam file
-samtools sort -o mapped.sorted.bam mapped.bam
-#use samtools to create an index file
-samtools index mapped.sorted.bam
-######idx stats?? check samtools mapping stats
-####samtools idxstats
-####samtools flagstat
-####samtools depth ###huge output
-####write function to get depth over a window instead of per site
+#use medaka to generate consensus seq and generate variants
+medaka_haploid_variant -t 2 trim.fastq *_ref.fasta
+#annotate medaka's vcf file so it can be uploaded into IGV
+medaka tools annotate consensus_to_ref.vcf *_ref.fasta mapped.sorted.bam variants.annotated.vcf
 
-#use Sniffles to call variants
-sniffles -m mapped.sorted.bam -v variants.vcf
-
+######### IGV LOOKS CRAZY WITH THIS OUTPUT DATA......... ????????? ERROR CORRECTION? FILTERING? RACON?
 #use IGV to visualize
-igv
+#igv
 #genomes --> upload the reference fasta file
 #file --> upload the vcf file (variants.vcf) AND the sorted bam file (mapped.sorted.bam)
+
+
+
+###### Alternative Map/Alignment using Minimap/Miniasm
+#use minimap2 to align sequencing reads onto themselves
+minimap2 -x ava-ont trim.fastq trim.fastq | gzip -1 > minimap.paf.gz
+#use the above minimap output and the trimmed reads to assemble untigs using miniasm
+miniasm -f trim.fastq minimap.paf.gz > miniasm.gfa
+
+###### Alternative Map/Alignment using Shasta
+#generates a folder called 'ShastaRun' that contains a file called Assembly.gfa
+shasta-Linux-0.7.0 --input trim.fastq
+
+#use Bandage to visualize different gfa assembly files
+Bandage
+#upload each of the gfa files into the Bandage gui
+
+
+
+########## THIS HAS NO ERROR CORRECTION ##############
+##use minimap2 to map to reference genome
+#minimap2 -ax map-ont --MD *_ref.fasta trim.fastq > mapped.sam
+##use samtools to convert sam to bam
+#samtools view -bS mapped.sam > mapped.bam
+#use samtools to sort the bam file
+#samtools sort -o mapped.sorted.bam mapped.bam
+##use samtools to create an index file
+#samtools index mapped.sorted.bam
+
+
 
 
 
